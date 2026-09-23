21a7ccb58c12b21918c40e1a0cd824e71c8a7d9e
Mathis Eloire <meloire@iutbayonne.univ-pau.fr>
2026-09-23 17:11:06 +0200

    ecriture de tests paramétrés

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,9 +1,24 @@
 package calculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CalculatorTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1, 1",
+            "1, 2, 3",
+            "-2, 2, 0",
+            "0, 0, 0",
+            "-1, -2, -3",
+    })
+    public void add_devrait_calculer_somme_deux_entier(int a, int b, int attendu){
+        assertThat(Calculator.add(a, b)).isEqualTo(attendu);
+    }
 
     @Test
     void add_devrait_calculer_somme_deux_entier()
